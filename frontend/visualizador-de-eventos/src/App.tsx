a7630e3e1a117c2cd85c3c388f5e61175dca3934
macpv <aldo.pintado.98@gmail.com>
2025-02-01 01:45:46 -0500

    feat: implementar formulario de login y registro

--- frontend/visualizador-de-eventos/src/App.tsx
+++ frontend/visualizador-de-eventos/src/App.tsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './views/Home'
+import Login from './views/Login'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </>
   )
